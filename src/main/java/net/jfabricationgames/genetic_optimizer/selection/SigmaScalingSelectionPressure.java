50a75d23e08dcbba9ab234f8263871fa9dd929f0
Tobias Faßbender <tobias-fassbender@gmx.de>
2019-05-13 17:31:33 +0200

    - implemented SelectionPressure subclass ProportionalSelectionPressure, that calculates the reproduction probability proportional to the fitness (with an optional offset) - tested the new class ProportionalSelectionPressure - added the classes SigmaScalingSelectionPressure and LinearDynamicScalingSelectionPressure (no implementations yet)

---
+++ src/main/java/net/jfabricationgames/genetic_optimizer/selection/SigmaScalingSelectionPressure.java
@@ -0,0 +1,12 @@
+package net.jfabricationgames.genetic_optimizer.selection;
+
+import net.jfabricationgames.genetic_optimizer.optimizer.DNA;
+
+public class SigmaScalingSelectionPressure implements SelectionPressure {
+	
+	@Override
+	public double[] calculateSelectionProbability(DNA[] population, int generation, boolean minimize, long timeUsed, long totalTime) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/test/java/net/jfabricationgames/genetic_optimizer/selection/ProportionalSelectionPressureTest.java
@@ -0,0 +1,110 @@
+package net.jfabricationgames.genetic_optimizer.selection;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+import net.jfabricationgames.genetic_optimizer.optimizer.DNA;
+
+class ProportionalSelectionPressureTest {
+	
+	@Test
+	public void testCalculateSelectionProbability_maximizeWithoutDefault() {
+		SelectionPressure selectionPressure = new ProportionalSelectionPressure();
+		
+		double[][] fitness = new double[][] {{0, 0, 1, 1, 0}, {1, 2, 3, 2, 2}, {1, 1, 1, 1}};
+		double[][] probabilities = new double[fitness.length][];
+		double[][] expectedProbabilities = new double[][] {{0, 0, 0.5, 0.5, 0}, {0.1, 0.2, 0.3, 0.2, 0.2}, {0.25, 0.25, 0.25, 0.25}};
+		//test all fitness arrays
+		for (int i = 0; i < fitness.length; i++) {
+			DNA[] population = new DNA[fitness[i].length];
+			for (int j = 0; j < population.length; j++) {
+				DNA dna = new DNA(0);
+				dna.setFitness(fitness[i][j]);
+				population[j] = dna;
+			}
+			
+			probabilities[i] = selectionPressure.calculateSelectionProbability(population, 0, false, 0, -1);
+		}
+		
+		double epsilon = 1e-5;
+		for (int i = 0; i < probabilities.length; i++) {
+			assertArrayEquals(expectedProbabilities[i], probabilities[i], epsilon);
+		}
+	}
+	
+	@Test
+	public void testCalculateSelectionProbability_maximizeWithDefault() {
+		SelectionPressure selectionPressure = new ProportionalSelectionPressure(3);
+		
+		double[][] fitness = new double[][] {{3, 3, 4, 4, 3}, {4, 5, 6, 5, 5}, {4, 4, 4, 4}};
+		double[][] probabilities = new double[fitness.length][];
+		double[][] expectedProbabilities = new double[][] {{0, 0, 0.5, 0.5, 0}, {0.1, 0.2, 0.3, 0.2, 0.2}, {0.25, 0.25, 0.25, 0.25}};
+		//test all fitness arrays
+		for (int i = 0; i < fitness.length; i++) {
+			DNA[] population = new DNA[fitness[i].length];
+			for (int j = 0; j < population.length; j++) {
+				DNA dna = new DNA(0);
+				dna.setFitness(fitness[i][j]);
+				population[j] = dna;
+			}
+			
+			probabilities[i] = selectionPressure.calculateSelectionProbability(population, 0, false, 0, -1);
+		}
+		
+		double epsilon = 1e-5;
+		for (int i = 0; i < probabilities.length; i++) {
+			assertArrayEquals(expectedProbabilities[i], probabilities[i], epsilon);
+		}
+	}
+	
+	@Test
+	public void testCalculateSelectionProbability_minimizeWithDefault() {
+		SelectionPressure selectionPressure = new ProportionalSelectionPressure(3);
+		
+		double[][] fitness = new double[][] {{3, 3, 2, 2, 3}, {2, 1, 0, 1, 1}, {2, 2, 2, 2}};
+		double[][] probabilities = new double[fitness.length][];
+		double[][] expectedProbabilities = new double[][] {{0, 0, 0.5, 0.5, 0}, {0.1, 0.2, 0.3, 0.2, 0.2}, {0.25, 0.25, 0.25, 0.25}};
+		//test all fitness arrays
+		for (int i = 0; i < fitness.length; i++) {
+			DNA[] population = new DNA[fitness[i].length];
+			for (int j = 0; j < population.length; j++) {
+				DNA dna = new DNA(0);
+				dna.setFitness(fitness[i][j]);
+				population[j] = dna;
+			}
+			
+			probabilities[i] = selectionPressure.calculateSelectionProbability(population, 0, false, 0, -1);
+		}
+		
+		double epsilon = 1e-5;
+		for (int i = 0; i < probabilities.length; i++) {
+			assertArrayEquals(expectedProbabilities[i], probabilities[i], epsilon);
+		}
+	}
+	
+	@Test
+	public void testCalculateSelectionProbability_minimizeWithoutDefault() {
+		SelectionPressure selectionPressure = new ProportionalSelectionPressure();
+		
+		double[][] fitness = new double[][] {{0, 0, -1, -1, 0}, {-1, -2, -3, -2, -2}, {-1, -1, -1, -1}};
+		double[][] probabilities = new double[fitness.length][];
+		double[][] expectedProbabilities = new double[][] {{0, 0, 0.5, 0.5, 0}, {0.1, 0.2, 0.3, 0.2, 0.2}, {0.25, 0.25, 0.25, 0.25}};
+		//test all fitness arrays
+		for (int i = 0; i < fitness.length; i++) {
+			DNA[] population = new DNA[fitness[i].length];
+			for (int j = 0; j < population.length; j++) {
+				DNA dna = new DNA(0);
+				dna.setFitness(fitness[i][j]);
+				population[j] = dna;
+			}
+			
+			probabilities[i] = selectionPressure.calculateSelectionProbability(population, 0, false, 0, -1);
+		}
+		
+		double epsilon = 1e-5;
+		for (int i = 0; i < probabilities.length; i++) {
+			assertArrayEquals(expectedProbabilities[i], probabilities[i], epsilon);
+		}
+	}
+}
